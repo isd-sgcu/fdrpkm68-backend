581e83947ae6aabf1d161edcc556c3969195e9b3
RookieJoel <jojackchanah@gmail.com>
2025-06-30 11:56:00 +0700

    Revert "init core user auth"
    This reverts commit 399c6acd7cffa71646efc4021c2ac0fa2745f5e9.

--- src/routes/userRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getMyProfile } from '../controllers/userController';
-import { authMiddleware, roleMiddleware } from '../middlewares/authMiddleware';
-
-const router = Router();
-router.get('/me', authMiddleware, getMyProfile); 
-
-export default router;
