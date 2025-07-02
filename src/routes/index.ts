e11168616f454bb42f318f952d1163bb6b26abe9
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-07-02 13:34:13 +0700

    feat: group routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authRoutes from './authRoutes';
 import userRoutes from './userRoutes';
 import houseRoutes from './houseRoutes';
+import groupRoutes from './groupRoutes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/house', houseRoutes);
+router.use('/groups', groupRoutes);
 
 export default router;
