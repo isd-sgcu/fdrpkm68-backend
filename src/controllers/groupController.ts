2b1212426459ad691bf7187f9a6a44bf5ca5cfed
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-07-04 15:37:53 +0700

    feat(groups): `/createOwnGroup` route

--- src/controllers/groupController.ts
+++ src/controllers/groupController.ts
@@ -1,10 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import * as groupService from "../services/groupService";
+import * as userService from "../services/userService";
+
 
 export const getGroupData = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const groupID = req.params.id;
         const houses = await groupService.getGroupDataFromDB(groupID)
+        console.log(houses);
         res.status(200).json({
             status: 'success',
             houses: houses
@@ -16,3 +19,47 @@ export const getGroupData = async (req: Request, res: Response, next: NextFuncti
             message: 'An error occurred while fetching group data.'
         })
     }}
+
+export const createOwnGroup = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+
+    if (!req?.user?.student_id || !req?.user?.citizen_id){
+      res.status(401).json({
+        status: 'error',
+        message: 'Unauthorized.'
+      })
+      return;
+    }
+
+    const user = await userService.findUserByStudentIdAndCitizenId(req.user.student_id,req.user.citizen_id);
+
+    if (!user) {
+      res.status(404).json({
+        status: 'error',
+        message: 'User not found.'
+      })
+      return;
+    }
+
+    if (user.group_id){
+      res.status(400).json({
+        status: 'error',
+        message: 'User already in a group, can\'t create additional groups.'
+      })
+      return;
+    }
+
+
+    const groupID = crypto.randomUUID();
+    await groupService.createOwnUserGroup(groupID, user);
+    res.status(201).json({
+      status: 'success',
+      newGroup: groupID
+    })
+  }catch (error){
+        res.status(500).json({
+            status: 'error',
+            message: 'An error occurred while creating group.'
+        })
+    }
+}
--- src/routes/groupRoutes.ts
+++ src/routes/groupRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getGroupData } from "../controllers/groupController";
+import { getGroupData,createOwnGroup} from "../controllers/groupController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.use(authMiddleware);
 
+router.post("/createOwnGroup", createOwnGroup);
 router.get("/:id", getGroupData);
 
 export default router;
