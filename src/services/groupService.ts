4b762d7471719bfad3189af6d3e74c692bb79ab0
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-07-04 15:37:33 +0700

    fix(groups): creating a own user group actually sets data in user table + change exported name

--- src/services/groupService.ts
+++ src/services/groupService.ts
@@ -1,6 +1,7 @@
 import { query } from "../database/client";
 import { CustomError } from "../types/error";
 import { Group } from "../types/group";
+import { User } from "../types/user";
 
 export const getGroupDataFromDB = async(groupID: string): Promise<(string | null)[]> => {
  try {
@@ -16,12 +17,18 @@ export const getGroupDataFromDB = async(groupID: string): Promise<(string | null
   }
 }
 
-export const createGroup = async(groupID: string): Promise<string> => {
+export const createOwnUserGroup = async(groupID: string, user: User): Promise<string> => {
   try {
+
+    // TODO: maybe split these up?
+    await query(`UPDATE users SET group_id = $1 WHERE student_id = $2 AND citizen_id = $3`, 
+      [groupID,user.student_id,user.citizen_id]
+    );
+    
     const result = await query(
       `INSERT INTO 
-      groups (group_id, submitted, selected_houses) 
-      VALUES ($1, FALSE, NULL)`,
+      groups (group_id, submitted) 
+      VALUES ($1, FALSE)`,
       [groupID]
     );
     return result.rows[0];
