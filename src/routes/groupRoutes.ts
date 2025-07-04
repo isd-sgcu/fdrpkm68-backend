ba47d2a3f536557366501bfbd943926748d12d04
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-07-04 14:43:59 +0700

    fix(groups): make group routes use auth middleware

--- src/routes/groupRoutes.ts
+++ src/routes/groupRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { getGroupData } from "../controllers/groupController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
+
+router.use(authMiddleware);
 
 router.get("/:id", getGroupData);
 
