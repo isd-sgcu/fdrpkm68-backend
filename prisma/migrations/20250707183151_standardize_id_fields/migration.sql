94691c2badca228ad56a9d5db7fb4629035a89c4
Sirawit Chanaburanasak <punpunsirawit@gmail.com>
2025-07-08 01:39:40 +0700

    feat: standardize ID fields

--- prisma/migrations/20250707183151_standardize_id_fields/migration.sql
+++ prisma/migrations/20250707183151_standardize_id_fields/migration.sql
@@ -21,6 +21,7 @@ CREATE TYPE "group_role_type" AS ENUM ('OWNER', 'MEMBER');
 
 -- CreateTable
 CREATE TABLE "users" (
+    "id" UUID NOT NULL,
     "student_id" VARCHAR(10) NOT NULL,
     "citizen_id" VARCHAR(13) NOT NULL,
     "prefix" "prefix_type" NOT NULL,
@@ -44,7 +45,7 @@ CREATE TABLE "users" (
     "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "users_pkey" PRIMARY KEY ("student_id")
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -61,32 +62,34 @@ CREATE TABLE "checkin" (
 
 -- CreateTable
 CREATE TABLE "house_sizes" (
+    "id" SERIAL NOT NULL,
     "size_letter" "housesize_letter_type" NOT NULL,
     "max_member" INTEGER NOT NULL,
 
-    CONSTRAINT "house_sizes_pkey" PRIMARY KEY ("size_letter")
+    CONSTRAINT "house_sizes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "houses" (
-    "house_id" SERIAL NOT NULL,
+    "id" SERIAL NOT NULL,
     "name_th" TEXT NOT NULL,
     "name_en" TEXT NOT NULL,
     "logo" TEXT NOT NULL,
     "description_th" TEXT NOT NULL,
     "description_en" TEXT NOT NULL,
-    "size_letter" "housesize_letter_type" NOT NULL,
+    "size_letter_id" INTEGER NOT NULL,
     "member_count" INTEGER NOT NULL DEFAULT 0,
     "max_member" INTEGER NOT NULL,
     "instagram" TEXT NOT NULL,
 
-    CONSTRAINT "houses_pkey" PRIMARY KEY ("house_id")
+    CONSTRAINT "houses_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "groups" (
-    "group_id" UUID NOT NULL,
+    "id" UUID NOT NULL,
     "submitted" BOOLEAN NOT NULL DEFAULT false,
+    "invite_code" TEXT NOT NULL,
     "house_rank_1" INTEGER,
     "house_rank_2" INTEGER,
     "house_rank_3" INTEGER,
@@ -94,35 +97,44 @@ CREATE TABLE "groups" (
     "house_rank_5" INTEGER,
     "house_rank_6" INTEGER,
 
-    CONSTRAINT "groups_pkey" PRIMARY KEY ("group_id")
+    CONSTRAINT "groups_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_student_id_key" ON "users"("student_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "checkin_user_student_id_event_key" ON "checkin"("user_student_id", "event");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "house_sizes_size_letter_key" ON "house_sizes"("size_letter");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "groups_invite_code_key" ON "groups"("invite_code");
+
 -- AddForeignKey
-ALTER TABLE "users" ADD CONSTRAINT "users_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "groups"("group_id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "users" ADD CONSTRAINT "users_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "groups"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "checkin" ADD CONSTRAINT "checkin_user_student_id_fkey" FOREIGN KEY ("user_student_id") REFERENCES "users"("student_id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "houses" ADD CONSTRAINT "houses_size_letter_fkey" FOREIGN KEY ("size_letter") REFERENCES "house_sizes"("size_letter") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "houses" ADD CONSTRAINT "houses_size_letter_id_fkey" FOREIGN KEY ("size_letter_id") REFERENCES "house_sizes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_1_fkey" FOREIGN KEY ("house_rank_1") REFERENCES "houses"("house_id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_1_fkey" FOREIGN KEY ("house_rank_1") REFERENCES "houses"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_2_fkey" FOREIGN KEY ("house_rank_2") REFERENCES "houses"("house_id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_2_fkey" FOREIGN KEY ("house_rank_2") REFERENCES "houses"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_3_fkey" FOREIGN KEY ("house_rank_3") REFERENCES "houses"("house_id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_3_fkey" FOREIGN KEY ("house_rank_3") REFERENCES "houses"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_4_fkey" FOREIGN KEY ("house_rank_4") REFERENCES "houses"("house_id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_4_fkey" FOREIGN KEY ("house_rank_4") REFERENCES "houses"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_5_fkey" FOREIGN KEY ("house_rank_5") REFERENCES "houses"("house_id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_5_fkey" FOREIGN KEY ("house_rank_5") REFERENCES "houses"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_6_fkey" FOREIGN KEY ("house_rank_6") REFERENCES "houses"("house_id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "groups" ADD CONSTRAINT "groups_house_rank_6_fkey" FOREIGN KEY ("house_rank_6") REFERENCES "houses"("id") ON DELETE SET NULL ON UPDATE CASCADE;
