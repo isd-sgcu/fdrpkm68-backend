391425f26f5d5116c324d8c5ae00007fdf0a3d5c
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-07-07 17:54:43 +0700

    docs(groups): add documentation for `/createOwnGroup`

--- src/routes/groupRoutes.ts
+++ src/routes/groupRoutes.ts
@@ -1,15 +1,53 @@
 import { Router } from "express";
-import { getGroupData,createOwnGroup,joinGroup,leaveGroup} from "../controllers/groupController";
+import {
+	getGroupData,
+	createOwnGroup,
+	joinGroup,
+	leaveGroup,
+} from "../controllers/groupController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.use(authMiddleware);
 
+/**
+ * @swagger
+ * /groups/createOwnGroup:
+ *  post:
+ *    summary: Create a new group for a user, if the user meets certain conditions.
+ *    description: Creates a new group for a user, if the user is NOT in ANY group (ANY group includes a 1-person group.)
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      201:
+ *        description: New group created successfully.
+ *        content:
+ *          application/json:
+ *           schema:
+ *           type: object
+ *           properties:
+ *             status:
+ *              type: string
+ *              example: success
+ *           message:
+ *              type: string
+ *           newGroup:
+ *              type: string
+ *              example: 7cd103c6-8b82-414c-8260-a1f03138354c
+ *      400:
+ *         description: Bad request, group not created, probably because user does not meet the conditions.
+ *      401:
+ *         description: Unauthorized, probably because user is not logged in/invalid credentials.
+ *      404:
+ *        description: User not found.
+ *      500:
+ *        description: Internal Server Error.
+ */
 router.post("/createOwnGroup", createOwnGroup);
+
 router.get("/:id", getGroupData);
 router.post("/join/:id", joinGroup);
 router.post("/leave", leaveGroup);
-
 
 export default router;
