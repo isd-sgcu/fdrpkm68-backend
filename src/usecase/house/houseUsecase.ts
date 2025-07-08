5fefd666ee141789f5e328216477c1442822a6d8
Borworntat Dendumrongkul <mastericez890@gmail.com>
2025-07-08 11:45:05 +0700

    feat: migrate Error to AppError

--- src/usecase/house/houseUsecase.ts
+++ src/usecase/house/houseUsecase.ts
@@ -1,6 +1,7 @@
 import { UUID } from "@/types/common";
 import { HouseRepository } from "@/repository/house/houseRepository";
 import { House } from "@/types/house/house";
+import { AppError } from "@/types/error/AppError";
 
 export class HouseUseCase {
   private houseRepository: HouseRepository;
@@ -14,10 +15,11 @@ export class HouseUseCase {
       const house = await this.houseRepository.getHouseById(id);
       return house;
     } catch (error) {
-      throw new Error(
+      throw new AppError(
         `Failed to get house by ID: ${
           error instanceof Error ? error.message : "Unknown error"
-        }`
+        }`,
+        500
       );
     }
   }
@@ -27,10 +29,11 @@ export class HouseUseCase {
       const houses = await this.houseRepository.getAllHouses();
       return houses;
     } catch (error) {
-      throw new Error(
+      throw new AppError(
         `Failed to get all houses: ${
           error instanceof Error ? error.message : "Unknown error"
-        }`
+        }`,
+        500
       );
     }
   }
