d511c845a8b2eae83c98dfa64c6d6caa0ba47679
Dawit <dawitsurithpinyo@gmail.com>
2025-07-13 15:49:48 +0700

    fix: add comments clarifying workshopTime and corresponding time intervals

--- src/usecase/rpkm/rpkmUsecase.ts
+++ src/usecase/rpkm/rpkmUsecase.ts
@@ -14,14 +14,21 @@ export class RpkmUsecase {
 
     async register(body: WorkshopRegisterRequest, userId: string): Promise<RPKMworkshop> {
         try{
-            if(body.workshopTime > 5 || body.workshopTime < 0){
-                throw new AppError("workshopTime must be integer from 0 to 5.", 400);
+            if(body.workshopTime > 6 || body.workshopTime < 1){
+                // 6 time slots for workshops
+                // slot 1: 13.00-13.30
+                // slot 2: 13.40-14.10
+                // slot 3: 14.20-14.50
+                // slot 4: 15.10-15.40
+                // slot 5: 15.50-16.20
+                // slot 6: 16.30-17.00
+                // For example, workshopTime: 5 -> register for slot 5
+                throw new AppError("workshopTime must be integer from 1 to 6.", 400);
             }
             if(!Object.values<string>(WorkshopType).includes(body.workshopType)){
                 throw new AppError("workshopType must be either 'DIFFUSER' or 'KEYCHAIN'.", 400);
             }
 
-            body.workshopTime += 1;
             const [workshopDuplicate, timeSlotDuplicate] = await this.rpkmRepository.getUserWorkshopConflicts(body, userId);
             if(workshopDuplicate.length >= 1){
                 throw new AppError(`User id ${userId} already registered for workshop ${body.workshopType}.`, 400);
